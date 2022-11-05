075c14621604f8255aef7e47e468f63ba7095aef
Aadhith Narayan M <aadhith2000@gmail.com>
2022-11-05 20:00:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,42 +26,40 @@ class Account:
 class SavingAccount(Account):
 	def __init__(self):
 		self.savingAccounts = {"11111" : ["hemil",1]}
+
 	def createAccount(self, name, initial):
 		self.accountNumber = randint(10000,99999)
 		self.savingAccounts[self.accountNumber] = [name, initial]
 		print ("Your account is successfully created, Your account number is {}".format(self.accountNumber))
+
 	def authenticate(self, name, acno):
-		accountNumber = acno
-		if accountNumber in self.savingAccounts.keys():
-			if self.savingAccounts[accountNumber][0] == name:
+		if acno in self.savingAccounts.keys():
+			if self.savingAccounts[acno][0] == name:
 				print ("Authentication Successful!")
-				self.accountNumber = accountNumber
+				self.accountNumber = acno
 			return True
 		else:
 			print ("all keys >>>>>>>>>>>>>" + str(self.savingAccounts.keys()))
 			print ("name you enter>>>>>>>>>" + name)
-			print ("name in database>>>>>>>" + self.savingAccounts[accountNumber][0])
+			print ("name in database>>>>>>>" + self.savingAccounts[acno][0])
 			print ("Authentication failed")
 			return False
 
 	def withdraw(self, wamt):
-		withdrawAmount = wamt
-		if withdrawAmount > self.savingAccounts[self.accountNumber][1]:
+		if wamt > self.savingAccounts[self.accountNumber][1]:
 			print ("Insufficient Balance")    
 		else:
-			self.savingAccounts[self.accountNumber][1] -= withdrawAmount
+			self.savingAccounts[self.accountNumber][1] -= wamt
 			print ("Withdraw Successful") 
 			self.displaybalance(self.accountNumber)
 
-	def deposite(self, damt):
-		depositeAmount = damt
-		self.savingAccounts[self.accountNumber][1] += depositeAmount
-		print ("deposite Successful")
+	def deposit(self, damt):
+		self.savingAccounts[self.accountNumber][1] += damt
+		print ("deposit Successful")
  		self.displaybalance(self.accountNumber)
 
 	def displaybalance(self, acno):
-		accountNumber = acno
-		print ("Available balance: {}".format(self.savingAccounts[accountNumber][1]))
+		print ("Available balance: {}".format(self.savingAccounts[acno][1]))
 
 
 savingAccount = SavingAccount()
@@ -91,8 +89,8 @@ while True:
 					withdrawAmount = int(raw_input("Enter withdrawAmount : "))
 					savingAccount.withdraw(withdrawAmount)
 				elif userChoice == 2:
-					depositeAmount = int(raw_input("Enter depositeAmount : "))
-					savingAccount.deposite(depositeAmount)
+					depositAmount = int(raw_input("Enter depositeAmount : "))
+					savingAccount.deposit(depositAmount)
 				elif userChoice == 3:
 					savingAccount.displaybalance(accountNumber)
 				elif userChoice == 4:
