810aef95f64c9283943f53ac8211d1382891c7b5
Aadhith Narayan M <aadhith2000@gmail.com>
2022-11-05 19:56:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,74 +3,78 @@ from random import randint
 
 class Account:
 	@abstractmethod
-	def createAccount():
+	def create_account(self):
 		return 0
 
 	@abstractmethod
-	def authenticate():
+	def authenticate(self):
 		return 0
 
 	@abstractmethod
-	def withdraw():
+	def withdraw(self):
 		return 0
 
 	@abstractmethod          
-	def deposite():
+	def deposit(self):
 		return 0
 
 	@abstractmethod
-	def displaybalance():
+	def display_balance(self):
 		return 0
 
 
 class SavingAccount(Account):
 	def __init__(self):
 		self.savingAccounts = {"11111" : ["hemil",1]}
-	def createAccount(self, name, initialDeposite):
+	def createAccount(self, name, initial):
 		self.accountNumber = randint(10000,99999)
-		self.savingAccounts[self.accountNumber] = [name, initialDeposite]
-		print "Your account is successfully created, Your account number is {}".format(self.accountNumber)
-	def authenticate(self, name, accountNumber):
+		self.savingAccounts[self.accountNumber] = [name, initial]
+		print ("Your account is successfully created, Your account number is {}".format(self.accountNumber))
+	def authenticate(self, name, acno):
+		accountNumber = acno
 		if accountNumber in self.savingAccounts.keys():
 			if self.savingAccounts[accountNumber][0] == name:
-				print "Authentication Successful!"
+				print ("Authentication Successful!")
 				self.accountNumber = accountNumber
 			return True
 		else:
-			print "all keys >>>>>>>>>>>>>" + str(self.savingAccounts.keys())
-			print "name you enter>>>>>>>>>" + name
-			print "name in database>>>>>>>" + self.savingAccounts[accountNumber][0]
-			print "Authentication failed"
+			print ("all keys >>>>>>>>>>>>>" + str(self.savingAccounts.keys()))
+			print ("name you enter>>>>>>>>>" + name)
+			print ("name in database>>>>>>>" + self.savingAccounts[accountNumber][0])
+			print ("Authentication failed")
 			return False
 
-	def withdraw(self, withdrawAmount):
+	def withdraw(self, wamt):
+		withdrawAmount = wamt
 		if withdrawAmount > self.savingAccounts[self.accountNumber][1]:
-			print "Insufficient Balance"    
+			print ("Insufficient Balance")    
 		else:
 			self.savingAccounts[self.accountNumber][1] -= withdrawAmount
-			print "Withdraw Successful" 
+			print ("Withdraw Successful") 
 			self.displaybalance(self.accountNumber)
 
-	def deposite(self, depositeAmount):
+	def deposite(self, damt):
+		depositeAmount = damt
 		self.savingAccounts[self.accountNumber][1] += depositeAmount
-		print "deposite Successful" 
+		print ("deposite Successful")
  		self.displaybalance(self.accountNumber)
 
-	def displaybalance(self, accountNumber):
-		print "Available balance: {}".format(self.savingAccounts[accountNumber][1])
+	def displaybalance(self, acno):
+		accountNumber = acno
+		print ("Available balance: {}".format(self.savingAccounts[accountNumber][1]))
 
 
 savingAccount = SavingAccount()
 
 while True:
-	print "Enter 1 to open an acc"
-	print "Enter 2 to acess existing account"
-	print "Enter 3 to exit"
+	print ("Enter 1 to open an acc")
+	print ("Enter 2 to acess existing account")
+	print ("Enter 3 to exit")
 	userChoice = int(raw_input(""))
 	if userChoice == 1:
 		name = raw_input("Enter your name: ")
-		initialDeposite = int(raw_input("Enter initialDeposite"))
-		savingAccount.createAccount(name,initialDeposite)
+		intial = int(raw_input("Enter Intial Deposit"))
+		savingAccount.createAccount(name,intial)
 
 	elif userChoice == 2:
 		name = raw_input("Enter name: ")
@@ -78,10 +82,10 @@ while True:
 		authenticateStatus = savingAccount.authenticate(name, accountNumber)
 		if authenticateStatus is True:
 			while True:
-				print "enter 1 to withdraw"
-				print "Enter 2 to deposite"
-				print "Enter 3 to displaybalance"
-				print "Enter 4 to exit"
+				print ("enter 1 to withdraw")
+				print ("Enter 2 to deposite")
+				print ("Enter 3 to displaybalance")
+				print ("Enter 4 to exit")
 				userChoice = int(raw_input(""))
 				if userChoice == 1:
 					withdrawAmount = int(raw_input("Enter withdrawAmount : "))
